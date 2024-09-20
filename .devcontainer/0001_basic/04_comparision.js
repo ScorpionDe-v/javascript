8e622dba71baefbde0138319b59dbf434c27f0a4
ScorpionDe-v <127703283+ScorpionDe-v@users.noreply.github.com>
2024-09-20 07:14:58 +0000

    comparisons of datatypes

---
+++ .devcontainer/0001_basic/04_comparision.js
@@ -0,0 +1,21 @@
+// console.log(2>1)
+// console.log(2>=1)
+// console.log(2<1)
+// console.log(2==1)
+// console.log(2!=1)
+
+
+//when we are not comparing the we will face dificulty
+
+// console.log("2">1)
+// console.log("02">1)
+
+console.log(null>0);
+console.log(null==0);
+console.log(null>=0);//comparisons convert to a  number,treating it as 0 that's why null>=0 true and null>0 is false
+
+console.log(undefined==0)
+console.log(undefined>=0)
+console.log(undefined<=0)
+//strict check
+console.log("2"===2)//=== check value as well as datatype
