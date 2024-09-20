4d39b7dffcb9e7287e60c0a2ccef1b1cce1c25b0
ScorpionDe-v <127703283+ScorpionDe-v@users.noreply.github.com>
2024-09-20 04:25:20 +0000

    datatypes

---
+++ .devcontainer/0001_basic/01_variable.js
@@ -0,0 +1,16 @@
+const accountId=123456
+let accountEmail="nakuldev15176j@gmail.com"
+var accountPassword="123456789"
+accountCity="Kanpur"
+let accountState
+
+// accountId=2 //not allowed
+accountEmail="nakuldev705222@gmail.com"
+accountPassword="987654321"
+accountCity="Lucknow"
+console.log(accountId);
+/*
+prefer not to use var becouse of issue in block scope and functional scope 
+
+*/
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
---
+++ .devcontainer/0001_basic/02_datatypes.js
@@ -0,0 +1,25 @@
+"use strict" //treat all javascript as newer version
+
+
+// alert("Hello"). we are using nodejs not browser
+
+// we will always maintain code readeability
+
+let name = "Nakul"
+let age =23
+let isLoggedIn =false
+let state;
+
+//types of primitive  datatypes in js
+//number =>2 to power 53
+//bigInt
+//string=>""
+//boolean=>true/false
+//null =>standalone value
+//undefined =>
+//symbol=> unique
+
+//object
+console.log(typeof "Nakul");
+console.log(typeof null)
+console.log(typeof undefined)
