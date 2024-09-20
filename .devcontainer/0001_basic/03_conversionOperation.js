839b8be9280bfb33cc9317e2a5ec8fa9c4700583
ScorpionDe-v <127703283+ScorpionDe-v@users.noreply.github.com>
2024-09-20 07:03:31 +0000

    conversionOperation2

--- .devcontainer/0001_basic/03_conversionOperation.js
+++ .devcontainer/0001_basic/03_conversionOperation.js
@@ -1,6 +1,6 @@
 let score="33"
-console.log(typeof score)
-console.log(typeof(score))
+// console.log(typeof score)
+// console.log(typeof(score))
 
 let valueInNumber = Number(score)
 // console.log(typeof valueInNumber)
@@ -10,5 +10,31 @@ let valueInNumber = Number(score)
 // true=1 ,false=0
 let someNumber=33;
 let stringNumber=String(someNumber)
-console.log(someNumber)
-console.log(typeof someNumber)
+// console.log(someNumber)
+// console.log(typeof someNumber)
+
+
+
+
+//some difficult situtations in js
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2**2)
+// console.log(2/3)
+// console.log(2%3)
+// console.log((2+3)-3*5/2%2)
+
+console.log(1+"2")
+console.log("1"+2)
+console.log(1+2+"3")
+console.log("1"+2+3)
+
+//not good practice
+
+console.log(+true)
+console.log(+"")
+let num1=num2=num3=2+2
+let gameCounter=100
+gameCounter++;
+console.log(gameCounter);
