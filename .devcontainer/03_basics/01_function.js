cb373ca6504d131408c72f725225dae022ae81da
ScorpionDe-v <127703283+ScorpionDe-v@users.noreply.github.com>
2024-09-25 10:48:16 +0000

    objects

--- .devcontainer/03_basics/01_function.js
+++ .devcontainer/03_basics/01_function.js
@@ -1,15 +1,47 @@
 //function definition
 function sayMyName(){
-  console.log("nakul");
-  console.log("dev");
-  console.log("jitu");
-  console.log("rohit");
+  // console.log("nakul");
+  // console.log("dev");
+  // console.log("jitu");
+  // console.log("rohit");
 
 }
 //sayMyName ---> functions reference
 // sayMyName()---> function excution
 sayMyName();
 
+// function addTwoNumbers(number1,number2){
+//      console.log(number1+number2)
+// }
+
 function addTwoNumbers(number1,number2){
-    number1+number2
+
+  // let result=number1+number2;
+  // return result
+
+  return number1+number2;
+  //console.log("Nakul");//after return in function nothing will excute and don't give the result
 }
+
+// addTwoNumbers(3,5)
+// addTwoNumbers(3+"a").  return argument and undefined
+// addTwoNumbers(4,null)
+const result=addTwoNumbers(3,5)
+
+// console.log("Result:",result);
+
+function loginUserMessage(username="Dev")
+{
+  // if(username===undefined)
+  if(!username)
+    {
+  console.log("please enter a username")
+  return 
+  }
+  return `${username} just logged in`
+}
+// console.log(loginUserMessage("Nakul"))
+// console.log(loginUserMessage(""))
+console.log(loginUserMessage())// when we define nothing then result will be undefined
+
+
