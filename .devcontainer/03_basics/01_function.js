c381fb718fe4125c46574d68e9b9e5a36f1764fc
ScorpionDe-v <127703283+ScorpionDe-v@users.noreply.github.com>
2024-09-25 10:12:23 +0000

    functions

---
+++ .devcontainer/03_basics/01_function.js
@@ -0,0 +1,15 @@
+//function definition
+function sayMyName(){
+  console.log("nakul");
+  console.log("dev");
+  console.log("jitu");
+  console.log("rohit");
+
+}
+//sayMyName ---> functions reference
+// sayMyName()---> function excution
+sayMyName();
+
+function addTwoNumbers(number1,number2){
+    number1+number2
+}
