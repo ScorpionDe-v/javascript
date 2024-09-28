907fc845748fc34bce0f14633113cf39ba213144
ScorpionDe-v <127703283+ScorpionDe-v@users.noreply.github.com>
2024-09-28 11:57:35 +0000

    truthyValues

---
+++ .devcontainer/04_logicControl/truthy.js
@@ -0,0 +1,13 @@
+const userEmail="nakul@gmail.com"
+
+if(userEmail)
+{
+    console.log("Got user email")
+}
+else{
+    console.log("Don't have user Email")
+}
+
+//falsy values.---> false ,0,-0,0n , undefined ,Nan
+//truthy value --> "0" ,'false' ," ", [] ,{} ,fuction(){} this is called empty function
+
