9f20ac69c5b02dd6ef8652a6855195a493fdfd5e
ScorpionDe-v <127703283+ScorpionDe-v@users.noreply.github.com>
2024-09-29 11:41:19 +0000

    forEachloop

---
+++ .devcontainer/05_iterations/forEachloop.js
@@ -0,0 +1,16 @@
+const coding =["js","ruby","java","python","cpp"]
+//callback  have. not name
+coding.forEach( function (val) {
+    // console.log(val);
+})
+
+coding.forEach((val)=>{
+    // console.log(val);
+})
+
+
+function printMe(item){
+    console.log(item);
+}
+// coding.forEach(printMe)
+
