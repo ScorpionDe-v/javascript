be308afec8e1ce45deaa6e9e2fd8a3d6141ecff0
ScorpionDe-v <127703283+ScorpionDe-v@users.noreply.github.com>
2024-09-29 09:26:28 +0000

    truthyValues

--- .devcontainer/04_logicControl/truthy.js
+++ .devcontainer/04_logicControl/truthy.js
@@ -11,3 +11,30 @@ else{
 //falsy values.---> false ,0,-0,0n , undefined ,Nan
 //truthy value --> "0" ,'false' ," ", [] ,{} ,fuction(){} this is called empty function
 
+const emptyObj={}
+if(Object.keys(emptyObj).length===0)
+{
+    console.log("object is empty");
+}
+
+//Nullish coalescing operator (??) : null undefined 
+
+let val1;
+// val1 = 5 ?? 10
+// val1=null ?? 10
+
+// val1 = undefined ?? 55
+
+val1=null??10??20
+
+// console.log(val1);
+
+//ternary operator
+
+// condtion ? true:false
+
+const iceTea=100;
+iceTea>=80? console.log("more than 80"): console.log("less than 80")
+
+
+
