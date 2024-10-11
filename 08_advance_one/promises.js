9b60f8404a10e6eab18e7432c0bdd3534226faf6
ScorpionDe-v <127703283+ScorpionDe-v@users.noreply.github.com>
2024-10-11 11:24:08 +0000

    promises

--- 08_advance_one/promises.js
+++ 08_advance_one/promises.js
@@ -76,6 +76,39 @@ const promiseFive=new Promise(function(resolve,reject){
             reject('ERROR:Dev went wrong')
         }
     },2000) 
-})
+});
 
-async function 
+async function consumePromiseFive(){
+  try {
+    const response = await promiseFive
+    console.log(response)
+    
+  } catch (error) {
+    console.log(error)
+    
+  }
+}
+consumePromiseFive()
+
+
+// async function getAllUsers() {
+// try {
+
+//    const response = await fetch('https://jsonplaceholder.typicode.com/users')
+//    const data = await response.json()
+//     console.log(data)
+    
+// } catch (error) {
+//  console.log("E:,error");
+    
+// }
+// };
+// getAllUsers()
+
+fetch('https://jsonplaceholder.typicode.com/users').then((response)=>{
+    return response.json()
+})
+.then((data)=>{
+    console.log(data);
+})
+.catch((error)=>console.log(error))
